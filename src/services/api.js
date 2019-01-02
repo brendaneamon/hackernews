18e672e4c597e189ef5143ff2d45b4641460532b
Brendan Magee <bmagee@gmail.com>
2019-01-01 22:12:51 -0500

    Feature/axios-implementation

--- src/services/api.js
+++ src/services/api.js
@@ -1,56 +1,37 @@
+import axios from 'axios'
+
 const BASE_PATH = 'http://hn.algolia.com/api/v1'
 const cache = new Map()
-
-const request = async (path, method = 'GET') => {
-  let response
-  try {
-    response = await window.fetch(path, {
-      method
-    })
-    return response
-  } catch (error) {
-    return Promise.reject(new Error(error))
-  }
-}
 
 const getSearchResults = async (term, currentPage) => {
   if (cache.has(term) && cache.get(term).page >= currentPage) {
     return Promise.resolve(cache.get(term))
   }
 
-  const response = await request(`${BASE_PATH}/search?query=${term}&page=${currentPage}`)
+  const searchEndpoint = `${BASE_PATH}/search?query=${term}&page=${currentPage}`;
+  const response = await axios.get(searchEndpoint)
 
   const {
-    status,
-    ok,
-    headers
+    data,
+    status
   } = response
 
-  if (!ok || status !== 200) {
+  if (status !== 200 || (data && !Array.isArray(data.hits))) {
     return Promise.reject(new Error(`Response code from Hacker News API: ${status}`))
   }
 
-  const isJSON = headers.get('content-type').includes('application/json')
-
-  if (isJSON) {
-    return response.json().then(data => {
-      if (cache.has(term)) {
-        const { hits: oldHits } = cache.get(term)
-        const result = {
-          hits: [...oldHits, ...data.hits],
-          page: currentPage
-        }
-        cache.set(term, result)
-        return result
-      }
-
-      const { hits, page } = data
-      cache.set(term, { hits, page })
-      return { hits, page }
-    })
+  if (cache.has(term)) {
+    const result = {
+      hits: [...cache.get(term).hits, ...data.hits],
+      page: currentPage
+    }
+    cache.set(term, result)
+    return result
   }
 
-  return Promise.reject(new Error('Response did not include JSON'))
+  const { hits, page } = data
+  cache.set(term, { hits, page })
+  return { hits, page }
 }
 
 
